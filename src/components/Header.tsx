4e25230059de446d5176f3d413322f4581dd92e1
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-04-16 15:52:31 +0200

    best score tracker

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { HeaderProps } from "../interfaces/Props";
 
-export default function Header({ score }: HeaderProps) {
+export default function Header({ score, best }: HeaderProps) {
     return (
-        <nav className="flex flex-row justify-between">
-            <h1 className="">Emomemo</h1>
-            <h3>Score: {score}</h3>
+        <nav className="flex flex-row justify-between items-center px-5">
+            <div className="flex justify-center items-center">
+                <img className="w-16" src="/images/cool.png" alt="" />
+                <h1 className="text-3xl">Emomemo</h1>
+            </div>
+            <div>
+                <h3 className="text-xl">Current Score: {score}</h3>
+                <h3 className="text-xl">Best Score: {best}</h3>
+            </div>
         </nav>
     );
 }
--- src/interfaces/Props.ts
+++ src/interfaces/Props.ts
@@ -1,5 +1,6 @@
 export interface HeaderProps {
     score: number;
+    best: number | null;
 }
 
 export interface GameProps {
