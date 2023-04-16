9f0c3bae2511c10bad1466d218af362471b6dd92
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-04-16 14:41:37 +0200

    Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { HeaderProps } from "../interfaces/Props";
 
-export default function Header() {
-    return <div>test header</div>;
+export default function Header({ score }: HeaderProps) {
+    return (
+        <nav className="flex flex-row justify-between">
+            <h1 className="">Emomemo</h1>
+            <h3>Score: {score}</h3>
+        </nav>
+    );
 }
