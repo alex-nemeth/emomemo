ee0aa7966e4266d7f7e0c92208df3052165b09ad
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-04-16 14:41:27 +0200

    cardData object with image details

---
+++ src/data/cardData.ts
@@ -0,0 +1,62 @@
+export default [
+    {
+        img: "/images/angel.png",
+        name: "Angel",
+    },
+    {
+        img: "/images/angry.png",
+        name: "Angry",
+    },
+    {
+        img: "/images/confused.png",
+        name: "Confused",
+    },
+    {
+        img: "/images/cool.png",
+        name: "Cool",
+    },
+    {
+        img: "/images/crying.png",
+        name: "Crying",
+    },
+    {
+        img: "/images/disappointed.png",
+        name: "Disappointed",
+    },
+    {
+        img: "/images/nerd.png",
+        name: "Nerd",
+    },
+    {
+        img: "/images/shhh.png",
+        name: "Shhh",
+    },
+    {
+        img: "/images/shrug.png",
+        name: "Shrug",
+    },
+    {
+        img: "/images/silly.png",
+        name: "Silly",
+    },
+    {
+        img: "/images/smile.png",
+        name: "Smile",
+    },
+    {
+        img: "/images/suprised.png",
+        name: "Suprised",
+    },
+    {
+        img: "/images/thinking.png",
+        name: "Thinking",
+    },
+    {
+        img: "/images/threaten.png",
+        name: "Threaten",
+    },
+    {
+        img: "/images/wink.png",
+        name: "Wink",
+    },
+];
